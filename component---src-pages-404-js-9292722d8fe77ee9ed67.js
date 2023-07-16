e3f4d2115680ffac0046e78752e68ff6c9998fd9
shameekagarwal <shameekagarwal@users.noreply.github.com>
2023-07-16 13:02:47 +0000

    deployed via Gatsby Publish Action 🎩 for 7e8e76f06a88b90a4b2c6736d1602e5abf51ad05

---
+++ component---src-pages-404-js-9292722d8fe77ee9ed67.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[883],{429:function(e,t,r){r.r(t);r(4619);var a=r(8369),l=r(7294),n=r(7650);t.default=function(){return l.createElement(n.A,{title:"404",path:"404"},l.createElement(a.Z.Title,{className:"_404--title"},"404"),l.createElement(a.Z.Paragraph,{level:1},"Sorry, the page you requested for wasn't found..."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9292722d8fe77ee9ed67.js.map
